93ec96d4b02618f78ee8b3798be1d9f97ad8a012
justin13242 <121122206+justin13242@users.noreply.github.com>
2022-12-20 21:24:17 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = (client) => {
+    return {
+        url: "https://discord.com/api/webhooks/1054882565523251350/T9c62ufhLd1i7tGZFlWDJPP5foTuqHHZFew0k4yyVsINmauN7JixiatAf-gkGeQ4nldR",
+    }
+}
